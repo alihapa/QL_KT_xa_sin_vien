a5d7d8cedd82b182a8838941d9670357d2ba1057
alihapa <btpx4869@gmail.com>
2026-03-31 08:58:17 +0700

    Add project files.

---
+++ QL_KT_xa_sin_vien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QL_KT_xa_sin_vien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
