42a44ee7e7627b3b0ccd01919600c45b56a4eb99
alihapa <btpx4869@gmail.com>
2026-04-18 12:04:16 +0700

    update-database

--- QL_KT_xa_sin_vien/Migrations/20260418050323_lan3.Designer.cs
+++ QL_KT_xa_sin_vien/Migrations/20260418050323_lan3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QL_KT_xa_sin_vien.Models;
 
@@ -11,9 +12,11 @@ using QL_KT_xa_sin_vien.Models;
 namespace QL_KT_xa_sin_vien.Migrations
 {
     [DbContext(typeof(QLSinhVienContext))]
-    partial class QLSinhVienContextModelSnapshot : ModelSnapshot
+    [Migration("20260418050323_lan3")]
+    partial class lan3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
